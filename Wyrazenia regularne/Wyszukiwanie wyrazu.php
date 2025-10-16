ee0fe7c47d6a7781cd79a35bcd9088203976545f
KrzysiekS21 <krzysztofek281@gmail.com>
2025-10-16 11:06:37 +0200

    16.10.2025

--- Wyrazenia regularne/Wyszukiwanie wyrazu.php
+++
@@ -1,9 +0,0 @@
-<p> Wyszukiwanie wyrozu w tekscie </p>
-<?php
-$wzorzec = '/być/';
-$tekst = "być albo nie być";
-if( preg_match( $wzorzec, $tekst ) ) 
-    echo"znaleziono wzorzec";
-else
-    echo "nie znaleziono wzorca";
-echo "znaleziono: " . preg_match_all( $tekst, $tekst );
